0602c78df0df49a4b7b74198a32ea11e337c3094
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2020-03-30 16:53:33 +0600

    Add files via upload

---
+++ mobile-UI/assets/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+
+     setTimeout(() => {
+         document.getElementById('splash').classList.toggle('fade');
+     }, 2000);
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
